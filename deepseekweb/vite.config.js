41ae94df7eecb7b1aae9ae47d93c80457d6172b6
offline-secure <seanlon.loh@gxbank.my>
2025-02-14 17:37:24 +0800

    update build path (#2)
    * Create static.yml
    
    * updatr
    
    * update build
    
    * udpate
    
    ---------
    
    Co-authored-by: dragonjump <116328950+dragonjump@users.noreply.github.com>

--- deepseekweb/vite.config.js
+++ deepseekweb/vite.config.js
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+  base: 'https://offline-secure.github.io/voice/deepseekweb/dist',
+ 
   plugins: [tailwindcss(), react()],
 });
